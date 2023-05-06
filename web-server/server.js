d6340b5af4b2fc64046a8e3462103f1719d1ba50
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2023-05-06 15:15:42 +0900

    dockerlizing

--- web-server/server.js
+++ web-server/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 5555;
 const cors = require("cors");
+require("dotenv").config();
 
 // Secret key used for signing and verifying tokens
-const secretKey =
-  "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs/1a20MD0H1qpwWL9zCU38V+Bt2D1gRKMaNfMrfqhw5SPYOl/sv1udOM1Wu7zwiCWB0qjdI6MpHf2R8A6130+Fe11soqg+3PJfdmJYFl6IrwOpOGk3fUEqEDYcVaDskWpQWy2YCB0qa9k5SBVpcAWNu/behS92HYYP7KE6d9txFyKll8wLfeP0RjNcaRz0u539nOiSZYm9RFPRyKg+wyKBukhujN3Dkh4sisHQwH7HWo5Ugn8p0oK8cuJRmkoGfFZ/juDRVsz6tEwbCQK74eu5ynl2W/iyGkSmCHirjYcj8PVyhL1esqimcbChAUhIa7Yfsb8Br3uN+Vq5A6W8nzyQIDAQAB\n-----END PUBLIC KEY-----";
+// Get from  KEY CLOAK ADMIN
+const keyCloak_pulic_key = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLIC_KEY_KEYCLOAK}\n-----END PUBLIC KEY-----`;
 
 const app = express();
 app.use(express.json());
@@ -21,15 +22,20 @@ function authenticateToken(req, res, next) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, secretKey, { algorithms: ["RS256"] }, (err, user) => {
-    if (err) {
-      res.status(403);
-      return res.send({ error: err });
-    }
+  jwt.verify(
+    token,
+    keyCloak_pulic_key,
+    { algorithms: ["RS256"] },
+    (err, user) => {
+      if (err) {
+        res.status(403);
+        return res.send({ error: err });
+      }
 
-    req.user = user;
-    next();
-  });
+      req.user = user;
+      next();
+    }
+  );
 }
 
 // Protected route that requires authentication
