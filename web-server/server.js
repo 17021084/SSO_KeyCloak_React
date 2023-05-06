acee0a42e76059de142e52c70ee91a62810bc98e
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2023-05-06 17:28:34 +0900

    setting docker

--- web-server/server.js
+++ web-server/server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const PORT = process.env.PORT || 5555;
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
+var bodyParser = require("body-parser");
 
+const PORT = process.env.PORT || 5555;
 // Secret key used for signing and verifying tokens
+
 // Get from  KEY CLOAK ADMIN
-const keyCloak_pulic_key = `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLIC_KEY_KEYCLOAK}\n-----END PUBLIC KEY-----`;
+var public_key = process.env.PUBLIC_KEY_KEYCLOAK || "";
+var keycloak_pulic_key = `-----BEGIN PUBLIC KEY-----\n${public_key}\n-----END PUBLIC KEY-----`;
+// const oldkey = keycloak_pulic_key;
 
 const app = express();
 app.use(express.json());
 // Enable CORS for all routes
 app.use(cors());
+// Set the Pug template engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Middleware to authenticate the token
 function authenticateToken(req, res, next) {
@@ -21,10 +32,13 @@ function authenticateToken(req, res, next) {
   if (token == null) {
     return res.sendStatus(401);
   }
+  // console.log("key", keycloak_pulic_key);
+  // console.log("old key", oldkey);
+  // console.log("compare key", oldkey == keycloak_pulic_key);
 
   jwt.verify(
     token,
-    keyCloak_pulic_key,
+    keycloak_pulic_key,
     { algorithms: ["RS256"] },
     (err, user) => {
       if (err) {
@@ -42,6 +56,21 @@ function authenticateToken(req, res, next) {
 app.get("/protected", authenticateToken, (req, res) => {
   res.send(["Trung", "Hanh", "Quang", "Hang", " Phuong Linh"]);
 });
+
+// api set key when run dockercompse  /setkey?key=
+// app.get("/setkey", (req, res) => {
+//   // public_key = req.query.key;
+//   // public_key = public_key.replace(/\s/g, "");
+//   // keycloak_pulic_key = `-----BEGIN PUBLIC KEY-----\n${public_key}\n-----END PUBLIC KEY-----`;
+//   // res.send(keycloak_pulic_key);
+//   res.render("form");
+// });
+
+// app.post("/settedkey", (req, res) => {
+//   public_key = req.body.name;
+//   keycloak_pulic_key = `-----BEGIN PUBLIC KEY-----\n${public_key}\n-----END PUBLIC KEY-----`;
+//   res.send(keycloak_pulic_key);
+// });
 
 app.get("/", (req, res) => {
   res.send("abc");
